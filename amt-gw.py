830358ef05d30e1e76f39bd9f2a5fde672707de6
natzberg <natalie.landsberg97@gmail.com>
2022-08-03 17:03:05 -0400

    implemented tunneling up to mem query

--- amt-gw.py
+++ amt-gw.py
@@ -1,86 +1,42 @@
 import socket
-import struct
-import sys
+from unittest.mock import DEFAULT
+from urllib import request
 from scapy.all import *
 from scapy.contrib.igmp import *
 from amt import *
-
-################################################
-# Various Lengths of Msgs or Hdrs
-################################################
-MAC_LEN = 6                # length of generated MAC in bytes
-NONCE_LEN = 4              # length of nonce in bytes
-MSG_TYPE_LEN = 1           # length of msg type 
-RELAY_QUERY_MSG_LEN = 48   # total length of relay query 
-RELAY_ADV_MSG_LEN = 12     # length of relay advertisement message 
-IGMP_QUERY_LEN = 24        # length of encapsulated IGMP query message 
-IGMP_REPORT_LEN = 20
-AMT_HDR_LEN = 2            # length of AMT header on a packet 
-IP_HDR_LEN = 20            # length of standard IP header 
-IP_HDR_IGMP_LEN = 24       # length of IP header with an IGMP report 
-UDP_HDR_LEN = 8            # length of standard UDP header 
-AMT_REQUEST_MSG_LEN = 9    # length of AMT request message
-AMT_DISCO_MSG_LEN = 8      # length of AMY discovery message
-
-################################################
-# Different AMT Message Types
-################################################
-AMT_RELAY_DISCO = 1        # relay discovery
-AMT_RELAY_ADV = 2          # relay advertisement
-AMT_REQUEST = 3            # reuqest
-AMT_MEM_QUERY = 4          # memebership query
-AMT_MEM_UPD = 5            # membership update
-AMT_MULT_DATA = 6          # multicast data
-AMT_TEARDOWN = 7           # teardown (not currently supported)
-
-MCAST_ANYCAST = "0.0.0.0"
-MCAST_ALLHOSTS = "224.0.0.22"
-LOCAL_LOOPBACK = "127.0.0.1"
-AMT_PORT = 2268
-
-DEFAULT_MTU = (1500 - (20 + 8))
+import secrets
 
 
-# message = 'very important data'.encode()
-# multicast_group = ('224.3.29.71', 10000)
-
-# # Create the datagram socket
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# # Set a timeout so the socket does not block indefinitely when trying
-# # to receive data.
-# sock.settimeout(0.2)
-# # Set the time-to-live for messages to 1 so they do not go past the
-# # local network segment.
-# ttl = struct.pack('b', 1)
-# sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-# try:
-
-#     # Send data to the multicast group
-#     print(sys.stderr, 'sending "%s"' % message)
-#     sent = sock.sendto(message, multicast_group)
-
-#     # Look for responses from all recipients
-#     while True:
-#         print(sys.stderr, 'waiting to receive')
-#         try:
-#             data, server = sock.recvfrom(16)
-#         except socket.timeout:
-#             print(sys.stderr, 'timed out, no more responses')
-#             break
-#         else:
-#             print(sys.stderr, 'received "%s" from %s' % (data, server))
-
-# finally:
-#     print(sys.stderr, 'closing socket')
-#     sock.close()
-
-ip_top_layer = IP(dst="192.168.1.1")
+ip_top_layer = IP(dst="162.250.137.254")        #relay addr
 igmp_layer = scapy.contrib.igmp.IGMP()
 udp_top_layer = UDP(sport=AMT_PORT, dport=AMT_PORT)
-amt_layer = AMT_Membership_Update()
+amt_layer = AMT_Discovery()
+nonce = secrets.token_bytes(4)
+amt_layer.setfieldval("nonce", nonce)
 igmp_layer = IGMP()
 # bind_layers(UDP, AMT_Discovery, dport=AMT_PORT)
 packet =  ip_top_layer / udp_top_layer / amt_layer
 # print(packet)
-send(packet)
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind(('', AMT_PORT))
+
+    # Send relay discovery message and wait for response
+    p = send(packet)
+    data, addr = s.recvfrom(DEFAULT_MTU)
+    relay_adv = AMT_Relay_Advertisement(data)   # convert raw packet to relay adv packet
+    print(relay_adv.fields)                     # just checking, debug
+
+    # once we receive the packet, craft a relay request
+    relay_request = AMT_Relay_Request()
+    relay_request.setfieldval("nonce", nonce)   # keep same nonce!
+    request_pkt = ip_top_layer / udp_top_layer / relay_request
+    p = send(request_pkt)
+    data, addr = s.recvfrom(DEFAULT_MTU)        # receive the membership query
+    print(data)
+    membership_query = AMT_Membership_Query(data)
+    print(membership_query.fields)
+
+    # received the membership query, now pull out the info we need
+
+            
--- amt.py
+++ amt.py
@@ -1,25 +1,21 @@
+"""
+    Implementation of the AMT protocol (https://datatracker.ietf.org/doc/html/rfc7450)
+    in Python. While AMT is designed to support both IPV4 and IPV6 traffic,
+    as of Aug 2022 Scapy does not fully support IPV6. This implementation relies on Scapy.
+"""
+
 from ctypes import sizeof
 import socket
 import struct
 import sys
 from scapy.all import *
+import scapy.contrib.igmpv3
 
 ################################################
 # Various Lengths of Msgs or Hdrs
 ################################################
-MAC_LEN = 6                # length of generated MAC in bytes
-NONCE_LEN = 4              # length of nonce in bytes
-MSG_TYPE_LEN = 1           # length of msg type 
-RELAY_QUERY_MSG_LEN = 48   # total length of relay query 
-RELAY_ADV_MSG_LEN = 12     # length of relay advertisement message 
-IGMP_QUERY_LEN = 24        # length of encapsulated IGMP query message 
-IGMP_REPORT_LEN = 20
-AMT_HDR_LEN = 2            # length of AMT header on a packet 
-IP_HDR_LEN = 20            # length of standard IP header 
-IP_HDR_IGMP_LEN = 24       # length of IP header with an IGMP report 
-UDP_HDR_LEN = 8            # length of standard UDP header 
-AMT_REQUEST_MSG_LEN = 9    # length of AMT request message
-AMT_DISCO_MSG_LEN = 8      # length of AMY discovery message
+VERSION_LEN = 4            # length of version in packet
+MSG_TYPE_LEN = 4           # length of msg type 
 
 ################################################
 # Different AMT Message Types
@@ -42,36 +38,30 @@ DEFAULT_MTU = (1500 - (20 + 8))
 class AMT_Discovery(Packet):
     name = "AMT_Discovery"
     fields_desc = [ 
-        BitField("version", 0, 4),
-        BitField("type", AMT_RELAY_DISCO, 4),
+        BitField("version", 0, VERSION_LEN),
+        BitField("type", AMT_RELAY_DISCO, MSG_TYPE_LEN),
         BitField("rsvd", 0, 24),
-        IntField("nonce", 0),
+        XStrFixedLenField("nonce", 0, 4)
     ]
 
 class AMT_Relay_Advertisement(Packet):
     name = "AMT_Relay_Advertisement"
     fields_desc = [
-        BitField("version", 0, 4),
-        BitField("type", AMT_RELAY_ADV, 4),
+        BitField("version", 0, VERSION_LEN),
+        BitField("type", AMT_RELAY_ADV, MSG_TYPE_LEN),
         BitField("rsvd", 0, 24),
-        IntField("nonce", 0),
-        MultipleTypeField(
-            [
-                (IPField("relay_addr", "0.0.0.0"), lambda pkt: pkt.len == 12),  #ipv4, UDP datagram length - 8 = 4
-                (IP6Field("relay_addr", "::"), lambda pkt: pkt.len == 20)       #ipv4, UDP datagram length - 8 = 16
-            ],
-            StrField("relay_addr", "")  #default
-        )
+        XStrFixedLenField("nonce", 0, 4),
+        IPField("relay_addr", MCAST_ANYCAST)
     ]
 class AMT_Relay_Request(Packet):
     name = "AMT_Relay_Request"
     fields_desc = [ 
-        BitField("version", 0, 4),
-        BitField("type", AMT_REQUEST, 4),
+        BitField("version", 0, VERSION_LEN),
+        BitField("type", AMT_REQUEST, MSG_TYPE_LEN),
         BitField("rsvd1", 0, 7),
         BitField("p_flag", 0, 1),
         BitField("rsvd2", 0, 16),
-        IntField("nonce", 0)
+        XStrFixedLenField("nonce", 0, 4)
     ]
 
 """
@@ -82,68 +72,105 @@ class AMT_Relay_Request(Packet):
 class AMT_Membership_Query(Packet):
     name = "AMT_Membership_Query"
     fields_desc = [
-        BitField("version", 0, 4),
-        BitField("type", AMT_MEM_QUERY, 4),
+        BitField("version", 0, VERSION_LEN),
+        BitField("type", AMT_MEM_QUERY, MSG_TYPE_LEN),
         BitField("rsvd1", 0, 6),
         BitField("l_flag", 0, 1),
         BitField("g_flag", 0, 1),
         BitField("rsvd2", 0, 16),
         MACField("response_mac", 0),
-        IntField("nonce", 0),
+        XStrFixedLenField("nonce", 0, 4),
         #encapsulated IGMPv3 or MLDv2, defaults to IGMP
-        BitField("igmp_mld_type", 0x11, 8),
-        ConditionalField(BitField("igmp_max_resp_code", 0, 8), lambda pkt: pkt.igmp_mld_type == 0x11 ),
-        ConditionalField(BitField("mld_code", 0, 0), lambda pkt: pkt.igmp_mld_type == 130),
-        XShortField("checksum", None),
-        ConditionalField(IPField("igmp_group_addr", "0.0.0.0"), lambda pkt: pkt.igmp_mld_type == 0x11),
-        ConditionalField(BitField("mld_rsvd1", 0, 16), lambda pkt: pkt.igmp_mld_type == 130),
-        ConditionalField(IP6Field("mld_addr", "::"), lambda pkt: pkt.igmp_mld_type == 130),
-        BitField("rsvd3", 0, 4),
-        BitField("s_flag", 0, 1),
-        BitField("qrv", 0, 3),
-        BitField("qqic", 0, 8),
-        IntField("num_of_srcs", 0),
-        ConditionalField(FieldListField("src_addrs", [], IPField("", "0.0.0.0"), count_from = lambda pkt: pkt.num_of_srcs), 
-            lambda pkt: pkt.igmp_mld_type == 0x11),
-        # ConditionalField(FieldListField("src_addrs", [], IPField("", "::"), count_from = lambda pkt: pkt.num_of_srcs), 
-        #     lambda pkt: pkt.igmp_mld_type == 130)
+        PacketListField("amt_igmpv3", None, scapy.contrib.igmpv3.IGMPv3)
+        # BitField("igmp_mld_type", 0x11, 8),
+        # ConditionalField(BitField("igmp_max_resp_code", 0, 8), lambda pkt: pkt.igmp_mld_type == 0x11 ),
+        # # ConditionalField(BitField("mld_code", 0, 0), lambda pkt: pkt.igmp_mld_type == 130),   #IPV6
+        # XShortField("checksum", None),
+        # ConditionalField(IPField("igmp_group_addr", MCAST_ANYCAST), lambda pkt: pkt.igmp_mld_type == 0x11),
+        # # ConditionalField(BitField("mld_rsvd1", 0, 16), lambda pkt: pkt.igmp_mld_type == 130),     #IPV6
+        # # ConditionalField(IP6Field("mld_addr", "::"), lambda pkt: pkt.igmp_mld_type == 130),       #IPV6
+        # BitField("rsvd3", 0, 4),
+        # BitField("s_flag", 0, 1),
+        # BitField("qrv", 0, 3),
+        # BitField("qqic", 0, 8),
+        # IntField("num_of_srcs", 0),
+        # ConditionalField(FieldListField("src_addrs", [], IPField("", MCAST_ANYCAST), count_from = lambda pkt: pkt.num_of_srcs), 
+        #     lambda pkt: pkt.igmp_mld_type == 0x11),
+        # # ConditionalField(FieldListField("src_addrs", [], IPField("", "::1"), count_from = lambda pkt: pkt.num_of_srcs), 
+        # #     lambda pkt: pkt.igmp_mld_type == 130) #IPV6
     ]
 
+"""
+    A gateway sends a Membership Update message to a relay to report a
+   change in group membership state, or to report the current group
+   membership state in response to receiving a Membership Query message.
+"""
 class AMT_Membership_Update(Packet):
     name = "AMT_Membership_Update"
 
-    igmp_mld_types = {
-                    0x11: "IGMP: Group Membership Query",
-                    0x12: "IGMP: Version 1 - Membership Report",
-                    0x16: "IGMP: Version 2 - Membership Report",
-                    0x17: "IGMP: Leave Group",
-                    130: "MLD: Multicast Listener Query",
-                    143: "MLD: Version 2 Multicast Listener Report",
-                    131: "MLD: Version 1 Multicast Listener Report",
-                    132: "MLD: Version 1 Multicast Listener Done"
-                }
+    igmptypes = {
+        0x11: "IGMP: Group Membership Query",
+        0x12: "IGMP: Version 1 - Membership Report",
+        0x16: "IGMP: Version 2 - Membership Report",
+        0x17: "IGMP: Leave Group"
+    }
+
+    # Currently not supported but here for future use
+    mldtypes = {
+        130: "MLD: Multicast Listener Query",
+        143: "MLD: Version 2 Multicast Listener Report",
+        131: "MLD: Version 1 Multicast Listener Report",
+        132: "MLD: Version 1 Multicast Listener Done"
+    }
 
     fields_desc = [
-        BitField("version", 0, 4),
-        BitField("type", AMT_MEM_UPD, 4),
+        BitField("version", 0, VERSION_LEN),
+        BitField("type", AMT_MEM_UPD, MSG_TYPE_LEN),
         BitField("rsvd1", 0, 8),
         MACField("response_mac", 0),
-        IntField("nonce", 0),
+        XStrFixedLenField("nonce", 0, 4),
         #encapsulated IGMPv3 or MLDv2, defaults to IGMP
-        ByteEnumField("igmp_mld_type", 0x16, igmp_mld_types),
-        ConditionalField(BitField("igmp_max_resp_code", 0, 8), lambda pkt: pkt.igmp_mld_type == 0x11 ),
-        ConditionalField(BitField("mld_code", 0, 0), lambda pkt: pkt.igmp_mld_type == 130),
-        XShortField("checksum", None),
-        ConditionalField(IPField("igmp_group_addr", "0.0.0.0"), lambda pkt: pkt.igmp_mld_type == 0x11),
-        ConditionalField(BitField("mld_rsvd1", 0, 16), lambda pkt: pkt.igmp_mld_type == 130),
-        ConditionalField(IP6Field("mld_addr", "::"), lambda pkt: pkt.igmp_mld_type == 130),
-        BitField("rsvd2", 0, 4),
-        BitField("s_flag", 0, 1),
-        BitField("qrv", 0, 3),
-        BitField("qqic", 0, 8),
-        IntField("num_of_srcs", 0),
-        ConditionalField(FieldListField("src_addrs", [], IPField("", "0.0.0.0"), count_from = lambda pkt: pkt.num_of_srcs), 
-            lambda pkt: pkt.igmp_mld_type == 0x11),
-        # ConditionalField(FieldListField("src_addrs", [], IPField("", "::"), count_from = lambda pkt: pkt.num_of_srcs), 
-        #     lambda pkt: pkt.igmp_mld_type == 130)
+        PacketListField("amt_igmpv3", None, scapy.contrib.igmpv3.IGMPv3)
+        # ByteEnumField("igmp_mld_type", 0x16, igmptypes),
+        # ConditionalField(BitField("igmp_max_resp_code", 0, 8), lambda pkt: pkt.igmp_mld_type == 0x11 ),
+        # # ConditionalField(BitField("mld_code", 0, 0), lambda pkt: pkt.igmp_mld_type == 130), #IPV6
+        # XShortField("checksum", None),
+        # ConditionalField(IPField("igmp_group_addr", MCAST_ANYCAST), lambda pkt: pkt.igmp_mld_type == 0x11),
+        # # ConditionalField(BitField("mld_rsvd1", 0, 16), lambda pkt: pkt.igmp_mld_type == 130), #IPV6
+        # # ConditionalField(IP6Field("mld_addr", "::"), lambda pkt: pkt.igmp_mld_type == 130),   #IPV6
+        # BitField("rsvd2", 0, 4),
+        # BitField("s_flag", 0, 1),
+        # BitField("qrv", 0, 3),
+        # BitField("qqic", 0, 8),
+        # IntField("num_of_srcs", 0),
+        # ConditionalField(FieldListField("src_addrs", [], IPField("", MCAST_ANYCAST), count_from = lambda pkt: pkt.num_of_srcs), 
+        #     lambda pkt: pkt.igmp_mld_type == 0x11),
+        # # ConditionalField(FieldListField("src_addrs", [], IPField("", "::"), count_from = lambda pkt: pkt.num_of_srcs), 
+        # #     lambda pkt: pkt.igmp_mld_type == 130) #IPV6
+    ]
+
+class AMT_Multicast_Data(Packet):
+    name = "AMT_Multicast_Data"
+    fields_desc = [
+        BitField("version", 0, VERSION_LEN),
+        BitField("type", AMT_MULT_DATA, 4),
+        BitField("rsvd", 0, 8),
+        PacketListField("amt_ip", None, scapy.layers.inet.IP)
+    ]
+
+"""
+    A gateway sends a Teardown message to a relay to request that it stop
+    sending Multicast Data messages to a tunnel endpoint created by an
+    earlier Membership Update message.
+"""
+class AMT_Teardown(Packet):
+    name = "AMT_Teardown"
+    fields_desc = [
+        BitField("version", 0, VERSION_LEN),
+        BitField("type", AMT_TEARDOWN, 4),
+        BitField("rsvd", 0, 8),
+        MACField("response_mac", 0),
+        XStrFixedLenField("nonce", 0, 4),
+        ShortField("gw_port_num", 0),
+        IPField("gw_ip_addr", MCAST_ANYCAST)
     ]
