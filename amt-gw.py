d22fc7d4c840f915ec53e562aa8304212505f80c
natzberg <natalie.landsberg97@gmail.com>
2022-08-04 16:54:01 -0400

    minor testing changes

--- amt-gw.py
+++ amt-gw.py
@@ -7,9 +7,9 @@ from amt import *
 import secrets
 import time
 
-def amt_mem_update(nonce, response_mac, udp_port):
+def amt_mem_update(nonce, response_mac):
     ip_layer = IP(dst="162.250.137.254")
-    udp_layer = UDP(sport=udp_port, dport=AMT_PORT)
+    udp_layer = UDP(sport=AMT_PORT, dport=AMT_PORT)
     amt_layer = AMT_Membership_Update()
     amt_layer.setfieldval("nonce", nonce)
     amt_layer.setfieldval("response_mac", response_mac)
@@ -20,13 +20,14 @@ def amt_mem_update(nonce, response_mac, udp_port):
     igmp_layer2 = IGMPv3mr(records=[IGMPv3gr(maddr='232.198.38.1', srcaddrs=["198.38.23.146"])])
     update = ip_layer / udp_layer / amt_layer / ip_layer2 / igmp_layer / igmp_layer2
     update.show()
-    send(update)
+    # send(update)
+    return update
 
 
 ip_top_layer = IP(dst="162.250.137.254")        #relay addr
-udp_rand_port = int(RandShort())
-print(udp_rand_port)
-udp_top_layer = UDP(sport=AMT_PORT ,dport=AMT_PORT)
+# udp_rand_port = RandShort()
+# print(udp_rand_port)
+udp_top_layer = UDP(sport=AMT_PORT, dport=AMT_PORT)
 amt_layer = AMT_Discovery()
 nonce = secrets.token_bytes(4)
 amt_layer.setfieldval("nonce", nonce)
@@ -34,32 +35,38 @@ amt_layer.setfieldval("nonce", nonce)
 packet =  ip_top_layer / udp_top_layer / amt_layer
 # print(packet)
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind(('', AMT_PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+s.bind(('', AMT_PORT))
 
-    # Send relay discovery message and wait for response
-    p = send(packet)
-    data, addr = s.recvfrom(DEFAULT_MTU)
-    relay_adv = AMT_Relay_Advertisement(data)   # convert raw packet to relay adv packet
-    print(relay_adv.fields)                     # just checking, debug
-    
-    # once we receive the packet, craft a relay request
-    relay_request = AMT_Relay_Request()
-    relay_request.setfieldval("nonce", nonce)   # keep same nonce!
-    udp_top_layer.sport = udp_rand_port
-    request_pkt = ip_top_layer / udp_top_layer / relay_request
-    p = send(request_pkt)
-    data, addr = s.recvfrom(DEFAULT_MTU)        # receive the membership query
-    print(data)
-    membership_query = AMT_Membership_Query(data)
-    response_mac = membership_query.response_mac
-    membership_query.show()
+# Send relay discovery message and wait for response
+p = send(packet)
+data, addr = s.recvfrom(DEFAULT_MTU)
+relay_adv = AMT_Relay_Advertisement(data)   # convert raw packet to relay adv packet
+print(relay_adv.fields)                     # just checking, debug
 
-    # received the membership query, send a membership update
-    amt_mem_update(nonce, response_mac, )
+# once we receive the packet, craft a relay request
+relay_request = AMT_Relay_Request()
+relay_request.setfieldval("nonce", nonce)   # keep same nonce!
+# udp_top_layer.sport = udp_rand_port
+request_pkt = ip_top_layer / udp_top_layer / relay_request
+p = send(request_pkt)
+data, addr = s.recvfrom(DEFAULT_MTU)        # receive the membership query
+print(data)
+membership_query = AMT_Membership_Query(data)
+response_mac = membership_query.response_mac
+membership_query.show()
 
-    while True:
-        # receive the multicast data!
-        data, addr = s.recvfrom(DEFAULT_MTU)        # receive the membership query
-        print(data)
+# received the membership query, send a membership update
+req = struct.pack("=4sl", socket.inet_aton("232.198.38.1"), socket.INADDR_ANY)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, req)
+# s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, 1)
+update = amt_mem_update(nonce, response_mac)
+send(update)
+# while True:
+    # receive the multicast data!
+data, addr = s.recvfrom(DEFAULT_MTU)        # receive the membership query
+print(data)
+s.close()
 
